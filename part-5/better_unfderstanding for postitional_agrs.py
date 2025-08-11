48cd2765d8656e8f59297b8bd9814c879a9ea9fd
routAshutoshhh <egoisticdev@gmail.com>
2025-08-11 18:11:48 +0530

    adding

--- part-5/better_unfderstanding for postitional_agrs.py
+++ part-5/better_unfderstanding for postitional_agrs.py
@@ -2,6 +2,8 @@ def arguments(*args , **kwargs):
     print("postional argument" , args)
     print("keyword argument" , kwargs)
 
+
+#creating list and dictionary to insert into the arguments
 lst = list(("sahin","kumar"))
 
 dictionary ={
@@ -12,8 +14,6 @@ dictionary ={
 
 
 arguments(lst, dictionary ) # this will  put every thing into the positional argument 
-
-
 print("the right way to do this:")
 #right way to  put the dictionary into keyword argument 
 arguments(*lst , **dictionary )
